82b3c5b56ad59cbf8c4418310cee8bb48a6d5d29
alikivasaki <alicevasaki@gmail.com>
2025-01-27 01:38:17 +0200

    Moving the declarations of the functions from admin to functions.h

--- classes/functions.h
+++ classes/functions.h
@@ -1,3 +1,16 @@
 /*
     General functions
 */
+#include <iostream>
+#include <string>
+#include "category_manager.h"
+#include "product_manager.h"
+using namespace std;
+
+string input_title();
+string input_description();
+string input_category_name(CategoryManager& categ_manager);
+string input_subcategory(CategoryManager& categ_manager, const string& category_name);
+float input_price();
+string input_measurement_type();
+int input_quantity(const string& measurement_type);
